b4de06a1b7d73bd3fcd2058d9137c87d3563affb
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-01 00:26:56 +0530

    add empty frontend files

---
+++ src/Includes/Actions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Paystack for Give | Frontend Actions File.
+ * 
+ * @package WordPress
+ * @subpackage Paystack for Give
+ * @since 1.0.0
+ */
+
+namespace Paystack\GiveWP\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Actions {
+    public function __construct() {
+        
+    }
+}
---
+++ src/Includes/Filters.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Paystack for Give | Frontend Filters File.
+ * 
+ * @package WordPress
+ * @subpackage Paystack for Give
+ * @since 1.0.0
+ */
+
+namespace Paystack\GiveWP\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Filters {
+    public function __construct() {
+        
+    }
+}
