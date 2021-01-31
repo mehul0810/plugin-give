c8215e944315887a3058f1a0eb5eb89bef6ca360
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-01 00:27:35 +0530

    add empty helpers file

---
+++ src/Includes/Helpers.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Paystack for Give | Helpers File.
+ * 
+ * @package WordPress
+ * @subpackage Paystack for Give
+ * @since 1.0.0
+ */
+
+namespace Paystack\GiveWP\Includes;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Helpers {
+    
+}
