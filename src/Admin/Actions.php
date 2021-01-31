9c6d0ae14d4bff74873d86aca53958eb08f9e312
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-01 00:26:46 +0530

    add empty admin files

---
+++ src/Admin/Actions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Paystack for Give | Admin Actions File.
+ * 
+ * @package WordPress
+ * @subpackage Paystack for Give
+ * @since 1.0.0
+ */
+
+namespace Paystack\GiveWP\Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Actions {
+    public function __construct() {
+        
+    }
+}
---
+++ src/Admin/Filters.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Paystack for Give | Admin Filters File.
+ * 
+ * @package WordPress
+ * @subpackage Paystack for Give
+ * @since 1.0.0
+ */
+
+namespace Paystack\GiveWP\Admin;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Filters {
+    public function __construct() {
+        
+    }
+}
