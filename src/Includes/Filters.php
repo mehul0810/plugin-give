5aca3fe063786eee5aed630f7eb9008c8faffc9a
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-26 01:24:50 +0530

    register gateways

--- src/Includes/Filters.php
+++ src/Includes/Filters.php
@@ -15,7 +15,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Filters {
+    /**
+     * Constructor.
+     * 
+     * @since  1.0.0
+     * @access public
+     * 
+     * @return void
+     */
     public function __construct() {
-        
+        add_filter( 'give_payment_gateways', [ $this, 'register_gateways' ] );
+    }
+
+    /**
+     * Register Gateways.
+     *
+     * @param array $gateways List of registered gateways.
+     * 
+     * @since  1.0.0
+     * @access public
+     * 
+     * @return array
+     */
+    public function register_gateways( $gateways ) {
+        $gateways['paystack'] = [
+            'admin_label'    => esc_html__( 'Paystack', 'paystack-give' ),
+            'checkout_label' => esc_html__( 'Paystack', 'paystack-give' ),
+        ];
+
+        return $gateways;
     }
 }
