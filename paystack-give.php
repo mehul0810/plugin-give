6be4ce431cccecce19302678cf34f91244ebf773
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-26 01:53:13 +0530

    improve plugin main file to use composer

--- paystack-give.php
+++ paystack-give.php
@@ -25,61 +25,19 @@
  * Domain Path:       /languages
  */
 
-// If this file is called directly, abort.
-if (! defined('WPINC') ) {
-    die;
+namespace Paystack\GiveWP;
+
+// Bailout, if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-/**
- * Currently plugin version.
- * Start at version 1.0.0 and use SemVer - https://semver.org
- * Rename this for your plugin and update it as you release new versions.
- */
-define('PLUGIN_NAME_VERSION', '1.2.0');
+// Load Constants.
+require_once __DIR__ . '/config/constants.php';
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-paystack-give-activator.php
- */
-function activate_paystack_give() 
-{
-    include_once plugin_dir_path(__FILE__) . 'includes/class-paystack-give-activator.php';
-    Paystack_Give_Activator::activate();
-}
+// Automatically loads files used throughout the plugin.
+require_once PAYSTACK_FOR_GIVE_PLUGIN_DIR . 'vendor/autoload.php';
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-paystack-give-deactivator.php
- */
-function deactivate_paystack_give() 
-{
-    include_once plugin_dir_path(__FILE__) . 'includes/class-paystack-give-deactivator.php';
-    Paystack_Give_Deactivator::deactivate();
-}
-
-register_activation_hook(__FILE__, 'activate_paystack_give');
-register_deactivation_hook(__FILE__, 'deactivate_paystack_give');
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path(__FILE__) . 'includes/class-paystack-give.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since 1.0.0
- */
-function run_paystack_give() 
-{
-
-    $plugin = new Paystack_Give();
-    $plugin->run();
-
-}
-run_paystack_give();
+// Initialize the plugin.
+$plugin = new Plugin();
+$plugin->register();
