a556556a7ef1f7ac8d2095cd0f4f79faceb8e3f5
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-26 01:13:09 +0530

    add get secret key helper fn

--- src/Includes/Helpers.php
+++ src/Includes/Helpers.php
@@ -15,5 +15,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class Helpers {
-    
+    /**
+     * Get Secret Key.
+     * 
+     * @since  1.0.0
+     * @access public
+     *
+     * @return string
+     */
+    public static function get_secret_key() {
+        $key = give_get_option( 'paystack_live_secret_key' );
+        
+        // Get test mode Secret Key.
+        if ( give_is_test_mode() ) {
+            $key = give_get_option( 'paystack_test_secret_key' );
+        }
+
+        return $key;
+    }
 }
