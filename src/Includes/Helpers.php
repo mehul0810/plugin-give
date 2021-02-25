243a3c68f6a80941fd68933583ea0938d9deb86c
Mehul Gohil <mehul.gohil0810@gmail.com>
2021-02-26 01:14:56 +0530

    add get public key helper fn

--- src/Includes/Helpers.php
+++ src/Includes/Helpers.php
@@ -26,11 +26,30 @@ class Helpers {
     public static function get_secret_key() {
         $key = give_get_option( 'paystack_live_secret_key' );
         
-        // Get test mode Secret Key.
+        // Get Secret Key for test mode.
         if ( give_is_test_mode() ) {
             $key = give_get_option( 'paystack_test_secret_key' );
         }
 
+        return $key;
+    }
+
+    /**
+     * Get Public Key.
+     * 
+     * @since  1.0.0
+     * @access public
+     *
+     * @return string
+     */
+    public static function get_public_key() {
+        $key = give_get_option( 'paystack_live_public_key' );
+        
+        // Get Public Key for test mode.
+        if ( give_is_test_mode() ) {
+            $key = give_get_option( 'paystack_test_public_key' );
+        }
+        
         return $key;
     }
 }
